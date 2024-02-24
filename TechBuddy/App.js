dcd4ce33471015045dfa80888853255664ff5b0b
rkwan05 <rkwan.hy@gmail.com>
2024-02-24 16:15:26 -0500

    quick tips initial backed and frontend setup

--- TechBuddy/App.js
+++ TechBuddy/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import EmergencyButton from "./components/EmergencyButton";
+import Tip from "./components/Tip";
 
 const App = () => {
+
   return (
     <View style={styles.container}>
+      <Tip style={{float: "right"}}/>
       <EmergencyButton />
       <StatusBar style="auto" />
     </View>
@@ -16,6 +19,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    // display: "flex"
   },
 });
 
