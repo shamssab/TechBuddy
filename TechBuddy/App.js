1b6849dca88a2758a59127e80dde362c4dd0e6fd
Vraj Shah <vrajr.shah@mail.utoronto.ca>
2024-02-29 20:39:52 -0500

    Added navigation bar

--- TechBuddy/App.js
+++ TechBuddy/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import EmergencyButton from "./components/EmergencyButton";
+import NavigationBar from "./components/NavigationBar";
 
 const App = () => {
+  const handleHomePress = () => {
+    console.log("Home Button Pressed");
+  };
+
+  const handleButton2Press = () => {
+    console.log("Button 2 pressed");
+  };
+
+  const handleButton3Press = () => {
+    console.log("Button 3 pressed");
+  };
+
   return (
     <View style={styles.container}>
       <EmergencyButton />
+      <NavigationBar>
+        onHomePress={handleHomePress}
+        onButton2Press={handleButton2Press}
+        onButton3Press={handleButton3Press}
+      </NavigationBar>
       <StatusBar style="auto" />
     </View>
   );
