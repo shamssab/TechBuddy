09247a260c3e058740c2c147c7d506131c5a3543
rkwan05 <rkwan.hy@gmail.com>
2024-03-16 22:20:35 -0400

    text-to-speech component, icon

--- TechBuddy/App.js
+++ TechBuddy/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import EmergencyButton from "./components/EmergencyButton";
 import Tip from "./components/Tip";
+import Speak from "./components/Speak";
 
 const App = () => {
 
   return (
     <View style={styles.container}>
+      <Speak text={"hello this page is reading aloud with a very long text so that I can test the pausing"}/>
       <Tip style={{float: "right"}}/>
       <EmergencyButton />
       <StatusBar style="auto" />
