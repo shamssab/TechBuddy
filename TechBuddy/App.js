ae352eeb19303109a5aadd2916db24f0010f5862
Krit Grover <kritgrover@gmail.com>
2024-03-17 13:59:47 -0400

    password manager import

--- TechBuddy/App.js
+++ TechBuddy/App.js
@@ -2,15 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import EmergencyButton from "./components/EmergencyButton";
 import Tip from "./components/Tip";
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import PasswordManager from "./components/PasswordManager";
 
 const App = () => {
-
   return (
     <View style={styles.container}>
-      <Tip style={{float: "right"}}/>
+      <Tip style={{ float: "right" }} />
       <EmergencyButton />
       <StatusBar style="auto" />
     </View>
+    // <SafeAreaView style={styles.container}>
+    //   <PasswordManager />
+    // </SafeAreaView>
   );
 };
 
